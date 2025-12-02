546d3899ebf1807cf26ea16aefd3d8cfc6d2e52f
dcsoft-ux <cdazaparra@gmail.com>
2025-12-02 10:22:33 -0500

    pull

--- 10_Funciones/10_4_Funciones.py
+++ 10_Funciones/10_4_Funciones.py
@@ -13,9 +13,32 @@ def llenar_lista(cantidad):
     #➡️ Devuelve la lista de números
     return lista_numeros
 
+#➡️ Funcion para saber que numeros son pares e impares en una lista
+def pares_e_impares(lista):
+    #➡️ Inicializa dos listas vacías para pares e impares
+    lista_pares = []
+    lista_impares = []
+    #➡️ Itera sobre cada número en la lista proporcionada
+    for numero in lista:
+        #➡️ Verifica si el número es par
+        if numero % 2 == 0:
+            #➡️ Agrega el número a la lista de pares
+            lista_pares.append(numero)
+        else:
+            #➡️ Agrega el número a la lista de impares
+            lista_impares.append(numero)
+    #➡️ Devuelve ambas listas: pares e impares
+    return lista_pares, lista_impares
+
 #➡️ Leer la cantidad de números a ingresar
 cantidad = input("¿Cuántos números deseas ingresar? ")
 #➡️ Llamar a la función llenar_lista y almacenar el resultado
 numeros = llenar_lista(cantidad)
 #➡️ Imprimir la lista de números ingresados
 print("Los números ingresados son:", numeros)
+#➡️ Llamar a la función pares_e_impares y almacenar los resultados
+pares, impares = pares_e_impares(numeros)
+#➡️ Imprimir las listas de números pares e impares
+print("Números pares:", pares)
+print("Números impares:", impares)
+
